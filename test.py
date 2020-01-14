b61f7764ab36266b3e59e3d1e9cd3f67139d2a62
scottwedge <scottwedge@users.noreply.github.com>
2020-01-13 23:28:35 -0500

    Update test.py
    Fixes #3.

--- test.py
+++ test.py
@@ -38,17 +38,17 @@ class Bank():
         #surname = input("Surname: ")
         #email = input("e-mail: ")
         password = input("password: ")
-        acountNumber = random.randint(100000,999999)
-        print("Account Number given: " +str(acountNumber))
-        return(name, password, acountNumber)
+        accountNumber = random.randint(100000,999999)
+        print("Account Number given: " +str(accountNumber))
+        return(name, password, accountNumber)
 
     def getIntoAccount(self):
-        promptAcount = input("Acount Number: ")
-        print(acountNumber)
+        promptAccount = input("Account Number: ")
+        print(accountNumber)
         promptPassword = input("Password: ")
-        if promptAcount == acountNumber:
+        if promptAccount == accountNumber:
             print("got one")
-            if promptPassword == passowrd:
+            if promptPassword == password:
                 print("you in")
 
     def deleteAccount(self):
@@ -57,6 +57,6 @@ class Bank():
 
 bank = Bank()
 
-name, password, acountNumber = bank.openAccount()
+name, password, accountNumber = bank.openAccount()
 bank.getIntoAccount()
 '''
